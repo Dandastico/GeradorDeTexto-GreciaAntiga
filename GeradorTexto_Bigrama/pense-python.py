c0cd162bdb87a3d8f80d941e547a91df31ca1ebd
Dandastico <dandastico.bsb@gmail.com>
2025-03-27 18:03:10 -0300

    consertei alguns bugs na função que abre arquivos

--- GeradorTexto_Bigrama/pense-python.py
+++ GeradorTexto_Bigrama/pense-python.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+
 '''
 Gerador de texto aleatório baseado no livro dr Jekyll
 Codificação escrita seguindo o passo a passo do livro Pense em Python
@@ -5,27 +9,41 @@ Codificação escrita seguindo o passo a passo do livro Pense em Python
 
 
 def main():
-    input_file = "dr_jekyll.txt"
+    input_file = "drjekyll.txt"
     output_file = "cln_txt.txt"
 
+    # checar se arquivo existe
+    if not os.path.exists(input_file):
+        sys.exit(f"Arquivo {input_file} não existe")
+        
     clean_file(input_file, output_file)
 
 
-def is_special_line(line):
-    # retira espaços em brancos, retorna None se tiver apenas espaços
-    line = line.strip()
-    # return só retorna o que não começar com *** ou None
-    return line.strip().startswith("*** ") or not line
+def is_section_marker(line):
+    '''se a linha começar com a marcação, retorna True'''
+    return line.strip().startswith("*** ")
 
 
 def clean_file(input_file, output_file):
+    '''Captura texto desejado pra análise de um arquivo .txt'''
+    # verifica se está na seção desejada do texto
+    targeted_section = False
+
     # abrir input como loop para ler linha por linha
-    with open(input_file, encoding="utf-8") as reader:
+    with open(input_file, 'r',encoding="utf-8") as reader:
         # abrir output como loop para escrever linha por linha
         with open(output_file, 'w', encoding="utf-8") as writer:
             for line in reader:
-                if not is_special_line(line):
+                # se a função retornar True
+                if is_section_marker(line):
+                    # inverte valor em targeted_section
+                    targeted_section = not targeted_section
+                    # "continue" garante que a linha com "*** " não seja escrito
+                    continue
+                # se o sinalizador da posição desejada for True
+                if targeted_section:    
                     writer.write(line)
 
 
-main()
+if __name__ == "__main__":
+    main()
